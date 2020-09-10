50f307917a68a6a3090fffd58b6c38d4ca5ff33b
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-09-10 19:04:36 +0900

    edges改善。fromUnfoldableには常にアノテーションが必要なのか？

--- src/Graph.purs
+++ src/Graph.purs
@@ -29,8 +29,8 @@ vertices (Vertex n)      = Set.singleton n
 vertices (Overlay g1 g2) = vertices g1 <> vertices g2
 vertices (Connect g1 g2) = vertices g1 <> vertices g2
 
--- edges :: Graph -> Set.Set (Tuple Int Int)
--- edges Empty           = Set.empty
--- edges (Vertex n)      = Set.empty
--- edges (Overlay g1 g2) = edges g1 <> edges g2
--- edges (Connect g1 g2) = (edges g1) <> (edges g2) <> ( Set.fromFoldable $ (\x y -> (Tuple x y)) <$> (Set.toUnfoldable $ vertices g1) <*> (Set.toUnfoldable $ vertices g2))
+edges :: Graph -> Set.Set (Tuple Int Int)
+edges Empty           = Set.empty
+edges (Vertex n)      = Set.empty
+edges (Overlay g1 g2) = edges g1 <> edges g2
+edges (Connect g1 g2) = (edges g1) <> (edges g2) <> ( Set.fromFoldable $ (\x y -> (Tuple x y)) <$> (Set.toUnfoldable  (vertices g1) :: Array Int) <*> (Set.toUnfoldable (vertices g2) :: Array Int))
