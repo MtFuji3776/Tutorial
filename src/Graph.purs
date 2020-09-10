7a6604f9db701aa1ceec02bc8ed09e009990ed44
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-09-10 20:06:27 +0900

    GraphのVertexを抽象化した。GraphをFunctorインスタンスにした。

--- src/Graph.purs
+++ src/Graph.purs
@@ -8,57 +8,64 @@ import Data.Semiring
 import Data.Set as Set
 import Data.Tuple
 import Data.Array
+import Data.Functor
 --import Data.List as List
 
-data Graph = Empty | Vertex Int | Overlay Graph Graph | Connect Graph Graph
+data Graph a = Empty | Vertex a | Overlay (Graph a) (Graph a) | Connect (Graph a) (Graph a)
 
-instance showGraph :: Show Graph where
+instance showGraph :: Show a => Show (Graph a) where
     show Empty           = "Empty"
     show (Vertex n)      = "Vertex " <> show n
     show (Overlay g1 g2) = "(" <> show g1 <> "+" <> show g2 <> ")"
     show (Connect g1 g2) = "(" <> show g1 <> "*" <> show g2 <> ")"
 
-instance semiringGraph :: Semiring Graph where
+instance semiringGraph :: Semiring (Graph a) where
     add g1 g2 = Overlay g1 g2
     zero      = Empty
     mul g1 g2 = Connect g1 g2
     one       = Empty
 
+instance functorGraph :: Functor Graph where
+    map f g = case g of Empty -> Empty
+                        Vertex x -> Vertex (f x)
+                        Overlay g1 g2 -> Overlay (map f g1) (map f g2)
+                        Connect g1 g2 -> Connect (map f g1) (map f g2)
+
 -- グラフから頂点集合を構成
-vertices :: Graph -> Set.Set Int
+vertices :: forall a . Ord a => Graph a -> Set.Set a
 vertices Empty           = Set.empty
 vertices (Vertex n)      = Set.singleton n
 vertices (Overlay g1 g2) = vertices g1 <> vertices g2
 vertices (Connect g1 g2) = vertices g1 <> vertices g2
 
 -- グラフから辺集合を構成
-edges :: Graph -> Set.Set (Tuple Int Int)
+edges :: forall a . Ord a => Graph a -> Set.Set (Tuple a a)
 edges Empty           = Set.empty
 edges (Vertex n)      = Set.empty
 edges (Overlay g1 g2) = edges g1 <> edges g2
-edges (Connect g1 g2) = edges g1 <> edges g2 <> (Set.fromFoldable $ (\x y -> Tuple x y) <$> (Set.toUnfoldable $ vertices g1 :: Array Int) <*> (Set.toUnfoldable $ vertices g2 :: Array Int))
+edges (Connect g1 g2) = edges g1 <> edges g2 <> (Set.fromFoldable $ (\x y -> Tuple x y) <$> (Set.toUnfoldable $ vertices g1 :: Array a) <*> (Set.toUnfoldable $ vertices g2 :: Array a))
 
 -- グラフを頂点集合と辺集合の組に変換
-destruct_ :: Graph -> Tuple (Set.Set Int) (Set.Set (Tuple Int Int))
+destruct_ :: forall a . Ord a => Graph a -> Tuple (Set.Set a) (Set.Set (Tuple a a))
 destruct_ g = Tuple (vertices g) (edges g)
 
 -- 実用上のデストラクタ。グラフを頂点と頂点タプルのArrayに変換
-destruct   :: Graph -> Tuple (Array Int) (Array (Tuple Int Int))
+destruct   :: forall a . Ord a => Graph a -> Tuple (Array a) (Array (Tuple a a))
 destruct g = let cross f g (Tuple x y) = Tuple (f x) (g y)
-             in cross (Set.toUnfoldable :: Set.Set Int -> Array Int) (Set.toUnfoldable :: Set.Set (Tuple Int Int) -> Array (Tuple Int Int)) $ destruct_ g
+             in cross (Set.toUnfoldable :: Set.Set a -> Array a) (Set.toUnfoldable :: Set.Set (Tuple a a) -> Array (Tuple a a)) $ destruct_ g
 
 -- 頂点Arrayから頂点のみのグラフを生成
-genVertices :: Array Int -> Graph
+genVertices :: forall a . Ord a => Array a -> Graph a
 genVertices ns = foldr Overlay Empty $ map Vertex ns
 
 -- 頂点タプルから辺だけ貼り合わせたグラフを生成
-genEdges :: Array (Tuple Int Int) -> Graph
+genEdges :: forall a . Ord a => Array (Tuple a a) -> Graph a
 genEdges ts = let toedge (Tuple x y) = Connect (Vertex x) (Vertex y) 
               in foldr Overlay Empty $ map toedge ts
 
 -- 頂点と頂点タプルのArrayからグラフを生成。
     -- destruct . graph . destruct = destruct
     -- destruct . graph = id が成立。
-graph :: Tuple (Array Int) (Array (Tuple Int Int)) -> Graph
+graph :: forall a . Ord a => Tuple (Array a) (Array (Tuple a a)) -> Graph a
 graph ts = let cross f g (Tuple x y) = Tuple (f x) (g y)
            in uncurry Overlay $ cross genVertices genEdges ts
