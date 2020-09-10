8d685accbf4357c91f4b3362ef07ef04e521bc49
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-09-10 16:03:56 +0900

    Algebraic Graphの動作を確認。edges関数がよくわからんエラーを吐く

--- src/Graph.purs
+++ src/Graph.purs
@@ -5,13 +5,15 @@ import Effect (Effect)
 import Effect.Console (log,logShow)
 import Math(sqrt)
 import Data.Semiring
-import Data.Set
+import Data.Set as Set
+import Data.Tuple
+--import Data.List as List
 
 data Graph = Empty | Vertex Int | Overlay Graph Graph | Connect Graph Graph
 
 instance showGraph :: Show Graph where
-    show Empty = "Empty"
-    show (Vertex n) = "Vertex " <> show n
+    show Empty           = "Empty"
+    show (Vertex n)      = "Vertex " <> show n
     show (Overlay g1 g2) = "(" <> show g1 <> "+" <> show g2 <> ")"
     show (Connect g1 g2) = "(" <> show g1 <> "*" <> show g2 <> ")"
 
@@ -21,14 +23,14 @@ instance semiringGraph :: Semiring Graph where
     mul g1 g2 = Connect g1 g2
     one       = Empty
 
-vertices :: Graph -> Set Int
-vertices Empty = empty
-vertices (Vertex n) = singleton n
+vertices :: Graph -> Set.Set Int
+vertices Empty           = Set.empty
+vertices (Vertex n)      = Set.singleton n
 vertices (Overlay g1 g2) = vertices g1 <> vertices g2
 vertices (Connect g1 g2) = vertices g1 <> vertices g2
 
-edges :: Graph -> Set (Tuple Int Int)
-edges Empty = empty
-edges (Vertex n) = empty
-edges (Overlay g1 g2) = edges g1 <> edges g2
-edges (Connect g1 g2) = edges g1 <> edges g2 <> fromList $ zip (toList $ vertices g1) (toList $ vertices g2)
+-- edges :: Graph -> Set.Set (Tuple Int Int)
+-- edges Empty           = Set.empty
+-- edges (Vertex n)      = Set.empty
+-- edges (Overlay g1 g2) = edges g1 <> edges g2
+-- edges (Connect g1 g2) = (edges g1) <> (edges g2) <> ( Set.fromFoldable $ (\x y -> (Tuple x y)) <$> (Set.toUnfoldable $ vertices g1) <*> (Set.toUnfoldable $ vertices g2))
